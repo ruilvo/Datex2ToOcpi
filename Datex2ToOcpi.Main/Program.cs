266bb3ae2e4cf313b0e0b264d21cb833fc2632e2
Rui Oliveira <ruimail24@gmail.com>
2025-09-25 15:22:30 +0100

    Add Main project

---
+++ Datex2ToOcpi.Main/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Datex2ToOcpi.Main
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
