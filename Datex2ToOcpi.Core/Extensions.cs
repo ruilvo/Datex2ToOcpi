4795754e1da28deb2ca206a41e456f97b979a315
Rui Oliveira <ruimail24@gmail.com>
2025-09-28 19:07:24 +0100

    Implement conversions as class extensions

---
+++ Datex2ToOcpi.Core/Extensions.cs
@@ -0,0 +1,85 @@
+﻿using Datex2ToOcpi.Core.Models.Datex2.EnergyInfrastructure;
+using OCM.Model.OCPI;
+
+using System;
+using System.IO;
+using System.Linq;
+using System.Xml.Serialization;
+
+namespace Datex2ToOcpi.Core;
+
+public static class ExtensionMethods
+{
+    /// <summary>
+    /// Combine the current EnergyInfrastructureSite with another one.
+    ///
+    /// Some API's have the static data and the dynamic data in separate endpoints.
+    /// Even though OCM only cares about the static data, the dynamic data is needed,
+    /// as it contains the current availability status, which includes whether the site
+    /// is operational or not.
+    /// </summary>
+    /// <param name="eiSite"></param>
+    /// <exception cref="NotImplementedException"></exception>
+    public static void CombineWith(this EnergyInfrastructureSite eiSite)
+    {
+        throw new NotImplementedException("Not yet implemented.");
+    }
+
+    /// <summary>
+    /// Converts an <see cref="EnergyInfrastructureSite"/> instance to an OCPI <see cref="Location"/>.
+    /// </summary>
+    /// <param name="eiSite">The <see cref="EnergyInfrastructureSite"/> to convert.
+    /// Cannot be <see langword="null"/>.
+    /// </param>
+    /// <returns>A new <see cref="Location"/> instance representing the OCPI location
+    /// mapped from the specified <see cref="EnergyInfrastructureSite"/>.
+    /// </returns>
+    public static Location ToOcpiLocation(this EnergyInfrastructureSite eiSite, Strategy.DefaultStrategy strategy)
+    {
+
+        Location location = new()
+        {
+            Country_code = strategy.CountryCode(eiSite),
+            Party_id = strategy.PartyId(eiSite),
+
+            // There seems to be no standard way of identifying a location in Datex II.
+            // This is a best-effort approach.
+            Id = eiSite.Id,
+
+            Publish = true,
+
+            Address = facilityLocation
+                      .Address
+                      .AddressLine[0]
+                      .Text
+                      .Values[0]
+                      .Value,
+
+            // The Spanish NAP is not following this correctly and so this value will
+            // probably be an empty string.
+            City = facilityLocation
+                   .Address
+                   .City
+                   .Values[0]
+                   .Value,
+
+            // Datex II uses two letter country codes, OCPI uses three letter codes.
+            Country = ISO3166.Country.List.First(
+                    c => c.TwoLetterCode == facilityLocation
+                                            .Address
+                                            .CountryCode
+                ).ThreeLetterCode,
+
+            Postal_code = facilityLocation
+                          .Address
+                          .Postcode,
+
+            Coordinates = new GeoLocation()
+            {
+                Latitude = eiSite.LocationReference.Point
+            }
+        };
+
+        return location;
+    }
+}
