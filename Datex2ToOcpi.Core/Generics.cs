1ef9a841f64d7eb92d7c496f0fffa151d608957a
Rui Oliveira <ruimail24@gmail.com>
2025-09-28 19:07:58 +0100

    Add generic helpers

---
+++ Datex2ToOcpi.Core/Generics.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+
+namespace Datex2ToOcpi.Core;
+
+public static class Generics
+{
+    public static void CombineObjects(object target, object other)
+    {
+        var type = target.GetType();
+
+        var properties = type.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+        foreach (var prop in properties)
+        {
+            if (!prop.CanRead || !prop.CanWrite)
+                continue;
+
+            var value = prop.GetValue(other);
+            if (value != null)
+            {
+                prop.SetValue(target, value);
+            }
+        }
+
+        var fields = type.GetFields(BindingFlags.Public | BindingFlags.Instance);
+        foreach (var field in fields)
+        {
+            var value = field.GetValue(other);
+            if (value != null)
+            {
+                field.SetValue(target, value);
+            }
+        }
+    }
+}
