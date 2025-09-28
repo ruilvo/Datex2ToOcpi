88aa9839f7669bb0ec996a22d846168fb26b78f1
Rui Oliveira <ruimail24@gmail.com>
2025-09-28 19:06:49 +0100

    Add helpers

---
+++ Datex2ToOcpi.Core/Helpers.cs
@@ -0,0 +1,16 @@
+﻿using Datex2ToOcpi.Core.Models.Datex2.EnergyInfrastructure;
+
+namespace Datex2ToOcpi.Core;
+
+public static class Helpers
+{
+    /// <summary>
+    /// The refill point uses the standard format: PT*KLC*E*ABF*00005*02
+    /// </summary>
+    /// <param name="refillPoint"></param>
+    /// <returns></returns>
+    public static string PartyIdFromRefillPoint(RefillPoint refillPoint)
+    {
+        return refillPoint.ExternalIdentifier.Substring(3, 3);
+    }
+}
